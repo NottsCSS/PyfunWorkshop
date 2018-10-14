f22dcf3da5c4e0dbba598fec2170da70b43618a7
chenwenkang <cwk1998@hotmail.com>
2018-10-15 05:27:44 +0800

    remove some excess

--- Lesson1/fbmessenger.py
+++ Lesson1/fbmessenger.py
@@ -12,19 +12,3 @@ for i in range(0,10):
     # user = user[0]
     client.send(Message(text="Lol Im spamming myself lol."), thread_id=client.uid)
 client.logout()
-
-# Docs: https://fbchat.readthedocs.io/en/master/api.html
-
-from fbchat import Client
-import getpass
-
-email = input("input your email:")
-password = getpass.getpass()
-client = Client(email, password)
-
-while True:
-    try:
-        pass
-    except KeyboardInterrupt:
-        client.logout()
-        break
