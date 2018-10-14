e2a129a0db5097bdb048ea7ad72789967fe60aaa
chenwenkang <cwk1998@hotmail.com>
2018-10-15 05:22:21 +0800

    initial commit

---
+++ Lesson1/fbmessenger.py
@@ -0,0 +1,30 @@
+# Docs: https://fbchat.readthedocs.io/en/master/api.html
+
+from fbchat import Client, Message
+import getpass
+
+email = input("input your email:")
+password = getpass.getpass()
+client = Client(email, password)
+
+for i in range(0,10):
+    # user = client.searchForUsers("alphonsuschen", limit=1)
+    # user = user[0]
+    client.send(Message(text="Lol Im spamming myself lol."), thread_id=client.uid)
+client.logout()
+
+# Docs: https://fbchat.readthedocs.io/en/master/api.html
+
+from fbchat import Client
+import getpass
+
+email = input("input your email:")
+password = getpass.getpass()
+client = Client(email, password)
+
+while True:
+    try:
+        pass
+    except KeyboardInterrupt:
+        client.logout()
+        break
