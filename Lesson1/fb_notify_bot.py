01e66c7ee052c65a15d92a4c74318c35fd34f256
chenwenkang <cwk1998@hotmail.com>
2018-10-16 03:17:55 +0800

    fix minor bug with displaying None as timestamp

--- Lesson1/fb_notify_bot.py
+++ Lesson1/fb_notify_bot.py
@@ -1,14 +1,15 @@
 # Docs: https://fbchat.readthedocs.io/en/master/api.html
 from threading import Thread
 import getpass
+import sys
 
 from fbchat import Client, Message, ThreadType
 from win10toast import ToastNotifier
 
 
 class CustomClient(Client):
-    def __init__(self, *args, **kwargs):
-        super(CustomClient, self).__init__(args, kwargs)
+    def __init__(self, email, password):
+        super(CustomClient, self).__init__(email=email, password=password)
         self.notifier = ToastNotifier()
 
     def onMessage(self, mid=None, author_id=None,
@@ -16,9 +17,9 @@ class CustomClient(Client):
                   thread_id=None, thread_type=ThreadType.USER,
                   ts=None, metadata=None, msg=None):
         thread_obj = self.fetchThreadInfo(thread_id)[str(thread_id)]
-        print(f"{thread_obj.name}({thread_obj.last_message_timestamp}) : {message}")
+        print(f"{thread_obj.name} : {message}")
         self.notifier.show_toast(
-            f"{thread_obj.name}({thread_obj.last_message_timestamp})",
+            thread_obj.name,
             message,
             icon_path=None,
             duration=5,
@@ -29,3 +30,9 @@ class CustomClient(Client):
 email = input("input your email:")
 password = getpass.getpass()
 client = CustomClient(email, password)
+try:
+        client.listen()
+except KeyboardInterrupt:
+    client.logout()
+    sys.exit(1)
+    print("exit!")
