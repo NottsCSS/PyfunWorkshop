c491dfedf4176276585740f75074faa0f7888345
chenwenkang <cwk1998@hotmail.com>
2018-10-22 03:57:44 +0800

    added requirements files to all the example folders

---
+++ Lesson3/ssg/pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'me'
+SITENAME = 'StaticSite'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kuala_Lumpur'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = './themes/pelican-alchemy/alchemy'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
