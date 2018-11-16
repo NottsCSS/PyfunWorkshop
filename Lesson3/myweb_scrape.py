2ad9b99f647440a5289c86a7e0303e9a10e09224
chenwenkang <cwk1998@hotmail.com>
2018-11-16 14:39:10 +0800

    added beginner example for web scraping

--- Lesson3/myweb_scrape.py
+++ Lesson3/myweb_scrape.py
@@ -2,3 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 url = "https://talk-93dae.firebaseapp.com/"
+
+connection = urlopen(url)
+html = connection.read()
+
+page = BeautifulSoup(html, "html.parser")
+
+content = page.find("h1")
+
+print(content.text)
